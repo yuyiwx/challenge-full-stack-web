ce37ccef88c1ae79fff7fb81fb0d4316bae456da
yuyiwx <yuyiwangxia@gmail.com>
2020-12-21 00:01:00 -0300

    changes on front

--- alunos-project/alunos-vuetify/src/views/Home.vue
+++ alunos-project/alunos-vuetify/src/views/Home.vue
@@ -1,7 +1,53 @@
 <template>
   <div class="home">
     <v-main class="ml-4">
-      <h1>Consultar Alunos</h1>
+      <h1 class="mb-6">Consultar Alunos</h1>
+      <v-dialog v-model="dialog" persistent max-width="600px">
+        <template class="mt-6" v-slot:activator="{ on, attrs }">
+          <v-btn color="primary" dark v-bind="attrs" v-on="on">
+            Cadastrar Usuário
+          </v-btn>
+        </template>
+        <v-card>
+          <v-card-title>
+            <span class="headline">Cadastro de Usário</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field
+                    label="Número de Registro*"
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field
+                    v-model="alunoName"
+                    label="Nome*"
+                    hint="example of helper text only on focus"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field
+                    label="CPF*"
+                    persistent-hint
+                    required
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+            </v-container>
+            <small>*campo obrigatório</small>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="dialog = false">
+              Close
+            </v-btn>
+            <v-btn color="blue darken-1" text @click="addAluno">Save</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
       <v-text-field
         v-model="alunoName"
         @keyup.enter="addAluno"
@@ -148,6 +194,7 @@ export default {
         },
       ],
       alunoName: "",
+      dialog: false,
     };
   },
   async created() {
